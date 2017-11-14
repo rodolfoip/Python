3ab88ab0920612d61ded6fd750ea378f221ed4fd
rodolfo <rodolfo.pereirailce@gmail.com>
2017-11-14 16:41:15 -0200

    Correção exercicios do curso em video

--- Desafios/desafio54.py
+++ Desafios/desafio54.py
@@ -1,8 +1,8 @@
 from datetime import date
 maiorIdade = 0
 menorIdade = 0
-for c in range(0,7):
-    anoNasc = int(input('Digite seu ano de nascimento: '))
+for c in range(1,8):
+    anoNasc = int(input('Digite o ano de nascimento da {}ª pessoa: '.format(c)))
     if (date.today().year - anoNasc) >= 18:
         maiorIdade +=1
     else:
--- Desafios/desafio55.py
+++ Desafios/desafio55.py
@@ -1,10 +1,14 @@
 maiorPeso = 0
-menorPeso = 9999
+menorPeso = 0
 for c in range(0, 5):
     peso = float(input('Escreva aqui seu peso: '))
-    if peso < menorPeso:
-        menorPeso = peso
-    elif peso > maiorPeso:
+    if c == 1:
         maiorPeso = peso
+        menorPeso = peso
+    else:
+        if peso < menorPeso:
+            menorPeso = peso
+        elif peso > maiorPeso:
+            maiorPeso = peso
 
 print('O menor peso medido foi {} e o maior {}'.format(menorPeso, maiorPeso))
